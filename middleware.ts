223b443e5003298853376648472505c8e50f775d
Kenys <kenisbohov@gmail.com>
2024-10-10 20:28:39 +0300

    middleware resolve problem

--- middleware.ts
+++ middleware.ts
@@ -2,74 +2,44 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from 'next/headers'
 import { jwtDecode } from 'jwt-decode';
 
-const allowedOrigins = ['*']
-
-const corsOptions = {
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-}
 
 export async function middleware(req: NextRequest) {
-
-    const origin = req.headers.get('origin') ?? ''
-    const isAllowedOrigin = allowedOrigins.includes(origin)
-
-    const isPreflight = req.method === 'OPTIONS'
-
-    if (isPreflight) {
-        const preflightHeaders = {
-            ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-            ...corsOptions,
-        }
-        return NextResponse.json({}, { headers: preflightHeaders })
-    }
-
-    const response = NextResponse.next()
-
-    if (isAllowedOrigin) {
-        response.headers.set('Access-Control-Allow-Origin', origin)
-    }
-
-    Object.entries(corsOptions).forEach(([key, value]) => {
-        response.headers.set(key, value)
-    })
-
-
-
+    let res = NextResponse.next()
+    
     const cookieStore = cookies()
     let authInfo: { isAuth: boolean, token: string } = JSON.parse(cookieStore.get('auth-info')?.value || '{}')
-
-
-
-    try {
-        if (authInfo && authInfo.isAuth === true) {
-            const backendRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-                method: 'POST',
-                credentials: 'include',
-                headers: new Headers(req.headers),
-
-            })
-            
-            if (backendRes.status === 201) {
-                const data = await backendRes.json()
-                response.headers.set('Set-Cookie', `${backendRes.headers.getSetCookie()}`)
-                response.cookies.set('auth-info', JSON.stringify({
-                    isAuth: true,
-                    userdata: jwtDecode(data.token),
-                    token: data.token
-                }), { maxAge: 1000 * 60, httpOnly: true })
-            }
-            else {
-                response.cookies.set('auth-info', JSON.stringify({
-                    isAuth: false,
-                    token: ''
-                }), { maxAge: 1000 * 60, httpOnly: true })
-                authInfo.isAuth = false
-            }
-        }
-    } catch (error) {
+    
+    const headers = new Headers()
+    headers.set('cookie', `${req.headers.get("cookie")}`)
+    if (authInfo && authInfo.isAuth === true) {
+        const backendRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: headers
+        })
         
+        if (backendRes.status === 201) {
+            const data = await backendRes.json()
+
+            res = NextResponse.next({
+                headers: new Headers({ 'Set-Cookie': `${backendRes.headers.getSetCookie()}` }),
+            })
+            const userdata = jwtDecode(data.token)
+            res.cookies.set('auth-info', JSON.stringify({
+                isAuth: true,
+                userdata: jwtDecode(data.token),
+                token: data.token
+            }), { maxAge: 1000 * 60, httpOnly: true })
+        }
+        else {
+            res.cookies.set('auth-info', JSON.stringify({
+                isAuth: false,
+                token: ''
+            }), { maxAge: 1000 * 60, httpOnly: true })
+            authInfo.isAuth = false
+        }
     }
+
 
     if (req.nextUrl.pathname === '/auth/login' && authInfo?.isAuth) {
         return NextResponse.redirect(new URL("/", req.url))
@@ -83,9 +53,7 @@ export async function middleware(req: NextRequest) {
         }
     }
 
-
-
-    return response
+    return res
 }
 
 export const config = {
