6f7e902f034924c3a2a8d532b40b5cc5a5562c9a
Kenys <kenisbohov@gmail.com>
2024-10-14 16:43:07 +0300

    try to fix middleware refresh token

--- middleware.ts
+++ middleware.ts
@@ -10,27 +10,29 @@ export async function middleware(req: NextRequest) {
     const cookieStore = cookies()
     let authInfo: { isAuth: boolean, token: string } = JSON.parse(cookieStore.get('auth-info')?.value || '{}')
 
-    const backendRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: req.headers
-    })
-    
-    if (backendRes.status === 201) {
-        const data = await backendRes.json()
-        response.headers.set('Set-Cookie', `${backendRes.headers.getSetCookie()}`)
-        response.cookies.set('auth-info', JSON.stringify({
-            isAuth: true,
-            userdata: jwtDecode(data.token),
-            token: data.token
-        }), { maxAge: 1000 * 60, httpOnly: true, secure: true })
-    }
-    else {
-        response.cookies.set('auth-info', JSON.stringify({
-            isAuth: false,
-            token: ''
-        }), { maxAge: 1000 * 60, httpOnly: true, secure: true })
-        authInfo.isAuth = false
+    if (authInfo && authInfo.isAuth) {
+        const backendRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: req.headers
+        })
+
+        if (backendRes.status === 201) {
+            const data = await backendRes.json()
+            response.headers.set('Set-Cookie', `${backendRes.headers.getSetCookie()}`)
+            response.cookies.set('auth-info', JSON.stringify({
+                isAuth: true,
+                userdata: jwtDecode(data.token),
+                token: data.token
+            }), { maxAge: 1000 * 60, httpOnly: true, secure: true })
+        }
+        else {
+            response.cookies.set('auth-info', JSON.stringify({
+                isAuth: false,
+                token: ''
+            }), { maxAge: 1000 * 60, httpOnly: true, secure: true })
+            authInfo.isAuth = false
+        }
     }
 
     if (req.nextUrl.pathname === '/auth/login' && authInfo?.isAuth) {
